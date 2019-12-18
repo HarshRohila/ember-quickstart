dc774366860b2454c77f1c78fb00e8c38cc5d348
Harsh Rohila <rohilaharsh@gmail.com>
2019-12-18 19:46:58 +0530

    added find all request for users

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,5 +1,13 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import { computed } from '@ember/object';
 
 export default JSONAPIAdapter.extend({
-  namespace: 'api/v1'
+  host: 'http://localhost:3000',
+  namespace: 'api/v1',
+
+  headers: computed(function() {
+    return {
+      'Content-Type': 'application/json'
+    };
+  })
 });
