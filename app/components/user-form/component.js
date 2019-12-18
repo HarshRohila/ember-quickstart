c6bbcccf10b053556a95e8d50b01d21ca37573b7
Harsh Rohila <rohilaharsh@gmail.com>
2019-12-18 20:23:49 +0530

    checking for username validity

--- app/components/user-form/component.js
+++ app/components/user-form/component.js
@@ -3,9 +3,16 @@ import Component from '@ember/component';
 export default Component.extend({
   userName: null,
 
+  isValid(userName) {
+    return !!userName;
+  },
+
   actions: {
     onSubmit() {
-      this.onSubmit(this.userName);
+      if (this.isValid(this.userName)) {
+        this.onSubmit(this.userName);
+        this.set('userName', '');
+      }
     }
   }
 });
